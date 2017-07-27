f89e3d4e2c4af1ee2356b543bed10c46423190fd
Ryan Cabanas <ryancabanas@users.noreply.github.com>
2017-07-27 12:33:35 -0400

    generics question 3

--- src/javadotlang/Main.java
+++ src/javadotlang/Main.java
@@ -5,6 +5,9 @@
  */
 package javadotlang;
 
+import java.util.Arrays;
+import java.util.List;
+
 
 
 /**
@@ -13,12 +16,34 @@ package javadotlang;
  */
 public class Main {
   public static void main(String[] args) {
-    String name = "Ryan Edward Cabanas";
-    String result = "";
-    for (int i = 0; i < name.length(); i++) {
-      if (Character.isUpperCase(name.charAt(i)))
-        result += name.charAt(i);
+    Integer[] ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    String[] strs = {"one", "two", "three"};
+//    try {
+//      Main.swapSpots(ints, 1, 8);
+//      for (Integer aInt : ints) {
+//        System.out.println(aInt);
+//      }
+//    } catch (Exception e) {
+//    }
+    try {
+      Main.swapSpots(strs, 0, 2);
+      for (String aStr : strs) {
+        System.out.println(aStr);
+      }
+    } catch (Exception e) {
     }
-    System.out.println(result);    
+    
+  }
+  
+  static <T> void swapSpots(T[] array, int index1, int index2) throws Exception {
+    if (index1 < 0 || index2 < 0 || index1 >= array.length || index2 >= array.length) {
+      System.out.println("One of the parameter indexes is outside the array bounds.");
+      throw new Exception();
+    }
+    T value1, value2;
+    value1 = array[index1];
+    value2 = array[index2];
+    array[index1] = value2;
+    array[index2] = value1;
   }
 }
